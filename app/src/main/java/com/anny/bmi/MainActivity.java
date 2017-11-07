4368d319ad41a7bc0f95072e13050320275bb05b
anny1002 <anny1002wade@gmail.com>
2017-11-07 10:56:06 +0800

    listener

--- app/src/main/java/com/anny/bmi/MainActivity.java
+++ app/src/main/java/com/anny/bmi/MainActivity.java
@@ -5,15 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String s = new String("abc");
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bHelp= (Button) findViewById(R.id.b_help);
+        bHelp.setOnClickListener(listener);
+        getResources().getString(R.string.app_name);
     }
     public void bmi(View view){
         //sout=System.out.println();
@@ -28,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         float bmi=weight / (height*height);
         Log.d("MainActivity","Your bmi is"+bmi);
         new AlertDialog.Builder(this)
-                .setMessage("Your BMI is" +bmi)
-                .setTitle("my title")
+                .setMessage(getResources().getString(R.string.your_bmi_is_) +bmi) //多國語言　抽取資料　對話框　button做事
+                .setTitle(R.string.bmi_title)
                 .setPositiveButton("OK",null)
                 .show();
       /*  Toast.makeText(this,
